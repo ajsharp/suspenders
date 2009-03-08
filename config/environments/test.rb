48c5502fcced6a4036b227a37232812dd328d2dc
Alex Sharp <ajsharp@gmail.com>
2009-03-07 23:48:19 -0600

    bumped gem versions to most recent available and added paperclip plugin

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,9 +21,9 @@ config.action_controller.allow_forgery_protection    = false
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
-config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com', :version => '>= 2.9.1'
+config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com', :version => '>= 2.10.1'
 config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com', :version => '>= 1.2.0'
-config.gem 'thoughtbot-quietbacktrace', :version => '>= 1.1.5', :lib => 'quietbacktrace'
+config.gem 'thoughtbot-quietbacktrace', :version => '>= 1.1.6', :lib => 'quietbacktrace'
 config.gem 'mocha', :version => '>= 0.9.5'
 
 HOST = 'localhost'
--- vendor/gems/thoughtbot-clearance-0.5.1/generators/clearance/templates/test/unit/user_test.rb
+++
@@ -1,5 +0,0 @@
-require 'test_helper'
-
-class UserTest < Test::Unit::TestCase
-  include Clearance::Test::Unit::UserTest
-end
