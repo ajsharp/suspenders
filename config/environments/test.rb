8b052e435136b53582a02dfe0b221b01aa8cdbec
Alex Sharp <ajsharp@gmail.com>
2009-04-17 18:31:58 -0500

    upgraded to rails 2.3.2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,7 +22,7 @@ config.action_controller.allow_forgery_protection    = false
 config.action_mailer.delivery_method = :test
 
 config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com', :version => '>= 2.10.1'
-config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com', :version => '>= 1.2.0'
+config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com', :version => '>= 1.2.1'
 config.gem 'thoughtbot-quietbacktrace', :version => '>= 1.1.6', :lib => 'quietbacktrace'
 config.gem 'mocha', :version => '>= 0.9.5'
 
