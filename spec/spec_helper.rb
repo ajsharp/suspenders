9c841acdea5e73cd8d824372023c005b0ed4e024
Alex Sharp <ajsharp@gmail.com>
2009-10-13 23:10:45 -0500

    added rspec files and spork-enabled spec_helper.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,34 @@
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However, 
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+  
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.dirname(__FILE__) + "/../config/environment"
+  require 'spec/autorun'
+  require 'spec/rails'
+  
+  Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+  
+  Spec::Runner.configure do |config|
+    config.use_transactional_fixtures = true
+    config.use_instantiated_fixtures  = false
+    config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
+  
+    # == Mock Framework
+    #
+    # RSpec uses it's own mocking framework by default. If you prefer to
+    # use mocha, flexmock or RR, uncomment the appropriate line:
+    #
+    # config.mock_with :mocha
+  end
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+  
+end
+
