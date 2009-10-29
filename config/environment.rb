6c75a7b1b1ce1f844d9908c25a70d52accde26b3
Alex Sharp <ajsharp@gmail.com>
2009-10-28 22:54:25 -0700

    cleaned up environment.rb a bit

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ throw "The project's name in environment.rb is blank" if PROJECT_NAME.empty?
 throw "Project name (#{PROJECT_NAME}) must_be_like_this" unless PROJECT_NAME =~ /^[a-z_]*$/
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -25,16 +25,13 @@ Rails::Initializer.run do |config|
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
   # Specify gems that this application depends on.
-  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com', :version => '~> 2.3.11'
-  config.gem 'thoughtbot-clearance', :lib => 'clearance', :version => '0.5.6'
+  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :version => '~> 2.3.11'
+  config.gem 'thoughtbot-clearance', :lib => 'clearance',     :version => '0.5.6'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-  
-  # Add the vendor/gems/*/lib directories to the LOAD_PATH
-  config.load_paths += Dir.glob(File.join(RAILS_ROOT, 'vendor', 'gems', '*', 'lib'))
 
   # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
@@ -44,16 +41,6 @@ Rails::Initializer.run do |config|
   # in the database in UTC, and return them converted to the specified local zone.
   # Run "rake -D time" for a list of tasks for finding time zone names. Uncomment to use default local time.
   config.time_zone = 'UTC'
-
-  # Your secret key for verifying cookie session data integrity.
-  # If you change this key, all old sessions will become invalid!
-  # Make sure the secret is at least 30 characters and all random, 
-  # no regular words or you'll be exposed to dictionary attacks.
-  SESSION_KEY = "29f5437d11e15cb8f4eb994c9d28202c261ea93f0fc9c4ecfc30830a1da39ab3a7ad834201785ea4fe266f5a598a7fd5965a4ec847e8a04a9fb1513729c3f817" 
-  config.action_controller.session = {
-    :session_key => "_#{PROJECT_NAME}_session",
-    :secret      => SESSION_KEY
-  }
 
   # Use the database for sessions instead of the cookie-based default,
   # which shouldn't be used to store highly confidential information
