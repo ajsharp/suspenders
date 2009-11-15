26fb0e1f2a5fbbb2d6b2be4457297a5c88ba3ea9
Alex Sharp <ajsharp@gmail.com>
2009-11-15 11:30:03 -0800

    upgraded to cucumber 0.4.3

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,6 +10,9 @@ module NavigationHelpers
     
     when /the home\s?page/
       '/'
+
+    when /the new user page/
+      new_user_url
     
     # Add more mappings here.
     # Here is a more fancy example:
