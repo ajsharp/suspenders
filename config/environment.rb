8ba8c84e621497a85efa030726ede6bb4de8d5d0
Alex Sharp <ajsharp@gmail.com>
2009-11-15 11:26:33 -0800

    changed project name to cvbeast

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 
 # Change this to the name of your rails project, like carbonrally.  
 # Just use the same name as the svn repo.
-PROJECT_NAME = "my_new_project"
+PROJECT_NAME = "cvbeast"
 
 throw "The project's name in environment.rb is blank" if PROJECT_NAME.empty?
 throw "Project name (#{PROJECT_NAME}) must_be_like_this" unless PROJECT_NAME =~ /^[a-z_]*$/
