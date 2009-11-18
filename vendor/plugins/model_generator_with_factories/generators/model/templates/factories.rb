29b5bd140dfd470dc6ca980fe6e3e25d731359a8
Alex Sharp <ajsharp@gmail.com>
2009-11-18 00:53:49 -0800

    removed some old plugins

--- vendor/plugins/model_generator_with_factories/generators/model/templates/factories.rb
+++
@@ -1,7 +0,0 @@
-Factory.define :<%= file_name %> do |f|
-<% unless attributes.empty? -%>
-<% for attribute in attributes -%>
-  f.<%= attribute.name %> <%= attribute.default_for_factory %>
-<% end -%>
-<% end -%>
-end
