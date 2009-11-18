53d2a3f4df932df47a5e0276f9b7605982295184
Alex Sharp <ajsharp@gmail.com>
2009-11-18 01:23:34 -0800

    removed login and active fields from users model

--- db/migrate/20090621150348_users_and_roles.rb
+++ db/migrate/20090621150348_users_and_roles.rb
@@ -2,14 +2,12 @@ class UsersAndRoles < ActiveRecord::Migration
   def self.up
     
     create_table :users do |t|
-      t.string    :login,               :null => false                # optional, you can use email instead, or both
       t.string    :email,               :null => false                # optional, you can use login instead, or both
       t.string    :crypted_password,    :default => nil, :null => true
       t.string    :password_salt,       :default => nil, :null => true                # optional, but highly recommended
       t.string    :persistence_token,   :null => false                # required
       t.string    :single_access_token, :null => false                # optional, see Authlogic::Session::Params
       t.string    :perishable_token,    :null => false                # optional, see Authlogic::Session::Perishability
-      t.boolean   :active,              :null => false, :default => false
       # Magic columns, just like ActiveRecord's created_at and updated_at. These are automatically maintained by Authlogic if they are present.
       t.integer   :login_count,         :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
       t.integer   :failed_login_count,  :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
@@ -18,10 +16,10 @@ class UsersAndRoles < ActiveRecord::Migration
       t.datetime  :last_login_at                                      # optional, see Authlogic::Session::MagicColumns
       t.string    :current_login_ip                                   # optional, see Authlogic::Session::MagicColumns
       t.string    :last_login_ip                                      # optional, see Authlogic::Session::MagicColumns
+
       t.timestamps
     end
     
-
     create_table :roles do |t|
       t.string :name
       t.timestamps
