6971a88d2bfe8e82244ce479c85bbfb6a46a6102
Alex Sharp <ajsharp@gmail.com>
2010-02-23 00:21:16 -0800

    nuked spork...i never use it

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,35 +1,25 @@
-require 'rubygems'
-require 'spork'
+ENV["RAILS_ENV"] ||= "cucumber"
+require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
+require 'cucumber/rails/world'
 
-Spork.prefork do
-  ENV["RAILS_ENV"] ||= "cucumber"
-  require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
-  require 'cucumber/rails/world'
-  
-  Cucumber::Rails::World.use_transactional_fixtures = true
-  
-  ActionController::Base.allow_rescue = false
-  
-  require 'cucumber'
-  # Comment out the next line if you don't want Cucumber Unicode support
-  #require 'cucumber/formatter/unicode'
-  require 'cucumber/webrat/element_locator' # Lets you do table.diff!(element_at('#my_table_or_dl_or_ul_or_ol').to_table)
-  require 'cucumber/rails/rspec'
-  
-  require 'webrat'
-  require 'webrat/core/matchers' 
-  Webrat.configure do |config|
-    config.mode = :rails
-    config.open_error_files = false # Set to true if you want error pages to pop up in the browser
-  end
+Cucumber::Rails::World.use_transactional_fixtures = true
 
-  require 'pickle/world'
+ActionController::Base.allow_rescue = false
+
+require 'cucumber'
+# Comment out the next line if you don't want Cucumber Unicode support
+#require 'cucumber/formatter/unicode'
+require 'cucumber/webrat/element_locator' # Lets you do table.diff!(element_at('#my_table_or_dl_or_ul_or_ol').to_table)
+require 'cucumber/rails/rspec'
+
+require 'webrat'
+require 'webrat/core/matchers' 
+Webrat.configure do |config|
+  config.mode = :rails
+  config.open_error_files = false # Set to true if you want error pages to pop up in the browser
 end
 
-Spork.each_run do
-  # This code will be run each time you run your specs.
-  
-end
+require 'pickle/world'
 
 
 # Example of configuring pickle:
