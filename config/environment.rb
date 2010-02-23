b84900013c99818129240bce917b35710fae9e27
Alex Sharp <ajsharp@gmail.com>
2010-02-23 00:22:09 -0800

    bumped to rails 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,10 @@
 # Just use the same name as the git repo.
 PROJECT_NAME = "project_name"
 
-throw "The project's name in environment.rb is blank" if PROJECT_NAME.empty?
 throw "Project name (#{PROJECT_NAME}) must_be_like_this" unless PROJECT_NAME =~ /^[a-z_]*$/
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
